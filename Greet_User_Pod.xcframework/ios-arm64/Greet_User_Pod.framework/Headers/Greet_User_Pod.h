71238b027301e2464f5a6a19d5cc9f4a9af57d10
Aanal Trivedi <aanal.trivedi@innodeed.com>
2023-10-25 15:15:02 +0530

    framework added

---
+++ Greet_User_Pod.xcframework/ios-arm64/Greet_User_Pod.framework/Headers/Greet_User_Pod.h
@@ -0,0 +1,18 @@
+//
+//  Greet_User_Pod.h
+//  Greet_User_Pod
+//
+//  Created by SAPPHIRE on 25/10/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Greet_User_Pod.
+FOUNDATION_EXPORT double Greet_User_PodVersionNumber;
+
+//! Project version string for Greet_User_Pod.
+FOUNDATION_EXPORT const unsigned char Greet_User_PodVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Greet_User_Pod/PublicHeader.h>
+
+
